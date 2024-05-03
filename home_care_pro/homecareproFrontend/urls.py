5b2862d1e57ceae1b2386c79fec8e32f927758cf
omaisafzal <126196753+omaiss@users.noreply.github.com>
2024-05-03 21:07:23 +0500

    Merged

--- home_care_pro/homecareproFrontend/urls.py
+++ home_care_pro/homecareproFrontend/urls.py
@@ -4,11 +4,8 @@ from .views import index
 urlpatterns = [
     path('', index),
     path('del_service', index),
-    path("home", index),
     path('login', index),
     path('add_service', index)
-
     path('home', index),
-    path('del_service', index),
     path('payment', index)
 ]
