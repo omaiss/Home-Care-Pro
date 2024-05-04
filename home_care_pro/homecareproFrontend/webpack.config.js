03fc46eb380236d15e29745eacecaba8500ccb7c
omaisafzal <126196753+omaiss@users.noreply.github.com>
2024-05-04 18:32:20 +0500

    Chatbot update

--- home_care_pro/homecareproFrontend/webpack.config.js
+++ home_care_pro/homecareproFrontend/webpack.config.js
@@ -30,13 +30,3 @@ module.exports = {
     }),
   ],
 };
-
-
-module.exports = {
-  output: {
-    filename: 'my-first-webpack.bundle.js',
-  },
-  module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
-  },
-};
